99aef85940c81a421b58fa5d0feba3a397dbcdc3
Jez Blackmore <jezblackmore@Jezs-MacBook-Air.local>
2022-01-27 10:49:51 +0000

    update

--- db/data/test-data/chatroom.js
+++ db/data/test-data/chatroom.js
@@ -1,8 +1,45 @@
 module.exports = [
-    {messages:['chat1', 'chat2', 'chat3']},
-    {messages:['chat1', 'chat2', 'chat3']},
-    {messages:['chat1', 'chat2', 'chat3']},
-    {messages:['chat1', 'chat2', 'chat3']},
-    {messages:['chat1', 'chat2', 'chat3']},
-    {messages:['chat1', 'chat2', 'chat3']}
+    {
+        "room_name": "2-5",
+        "users_in_chat": ['{2}', '{5}'],
+        "messages": [
+            "esse dolore proident non anim cillum qui non pariatur laboris eu ad labore irure voluptate eiusmod",
+            "aliquip mollit ad aute enim commodo"
+        ]
+    },
+    {
+        "room_name": "3-6",
+        "users_in_chat": ['{3}', '{6}'],
+        "messages": [
+            "esse dolore proident non anim cillum qui non pariatur laboris eu ad labore irure voluptate eiusmod",
+            "aliquip mollit ad aute enim commodo",
+            "reprehenderit exercitation ipsum sit excepteur et fugiat esse occaecat amet proident occaecat aliqua exercitation aliqua et officia tempor"
+        ]
+    },
+    {
+        "room_name": "4-7",
+        "users_in_chat": ['{4}', '{7}'],
+        "messages": [
+            "esse dolore proident non anim cillum qui non pariatur laboris eu ad labore irure voluptate eiusmod",
+            "aliquip mollit ad aute enim commodo",
+            "reprehenderit exercitation ipsum sit excepteur et fugiat esse occaecat amet proident occaecat aliqua exercitation aliqua et officia tempor"
+        ]
+    },
+    {
+        "room_name": "8-9",
+        "users_in_chat": ['{8}', '{9}'],
+        "messages": [
+            "esse dolore proident non anim cillum qui non pariatur laboris eu ad labore irure voluptate eiusmod",
+            "aliquip mollit ad aute enim commodo"
+        ]
+    },
+    {
+        "room_name": "9-10",
+        "users_in_chat": ['{9}', '{10}'],
+        "messages": [
+            "esse dolore proident non anim cillum qui non pariatur laboris eu ad labore irure voluptate eiusmod",
+            "aliquip mollit ad aute enim commodo",
+            "reprehenderit exercitation ipsum sit excepteur et fugiat esse occaecat amet proident occaecat aliqua exercitation aliqua et officia tempor"
+        ]
+    }
 ]
