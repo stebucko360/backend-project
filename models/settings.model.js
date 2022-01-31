07891aa83c0d296c4ef219b90ec98094731be542
stebucko360 <stebucko360@gmail.com>
2022-01-31 15:40:10 +0000

    updated end point for settings changes, can now update all user settings together or individually

--- models/settings.model.js
+++ models/settings.model.js
@@ -3,19 +3,47 @@ const geocoder = require("../utils/nodeGeocoder");
 
 // PATCH: /api/settings/:user_id
 
-exports.updateSettingsPostcode = async (settings_postcode, user_id) => {
+exports.updateSettingsPostcode = async (
+  settings_postcode = "M17ED",
+  user_id,
+  settings_min_price = 0,
+  settings_max_price = 300000,
+  settings_house_type = "house",
+  settings_radius = 5
+) => {
+  const postCodeRegEx = /^([A-Z]{1,2}\d[A-Z\d]? ?\d[A-Z]{2}|GIR ?0A{2})$/gi;
+  if (!postCodeRegEx.test(settings_postcode)) {
+    return Promise.reject({ status: 400, msg: "Invalid postcode" });
+  }
 
-  const addressDataResults = await Promise.all([geocoder.geocode(settings_postcode)]); 
+  const addressDataResults = await Promise.all([
+    geocoder.geocode(settings_postcode),
+  ]);
 
-  return db.query(
-        `UPDATE users 
+  return db
+    .query(
+      `UPDATE users 
           SET 
             settings_postcode = $1, 
             settings_latitude = $2,
-            settings_longitude = $3
-          WHERE user_id = $4 RETURNING *;`,
-        [addressDataResults[0][0].zipcode, addressDataResults[0][0].latitude, addressDataResults[0][0].longitude , user_id]
-      ).then((response) => {
-        return response.rows[0];
+            settings_longitude = $3,
+            settings_price_min = $4,
+            settings_price_max = $5,
+            settings_house_type = $6,
+            settings_radius = $7
+          WHERE user_id = $8 RETURNING *;`,
+      [
+        addressDataResults[0][0].zipcode,
+        addressDataResults[0][0].latitude,
+        addressDataResults[0][0].longitude,
+        settings_min_price,
+        settings_max_price,
+        settings_house_type,
+        settings_radius,
+        user_id,
+      ]
+    )
+    .then((response) => {
+      return response.rows[0];
     });
 };
